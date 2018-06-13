27b59f883abd24fd979ed93d88377aef88618d1d
Philip Ribbens <philip.ribbens@gmail.com>
2018-06-13 15:17:07 -0400

    Test commit

--- eco_valuator_provider.py
+++ eco_valuator_provider.py
@@ -54,7 +54,7 @@ class EcoValuatorProvider(QgsProcessingProvider):
         Loads all algorithms belonging to this provider.
         """
         for alg in self.alglist:
-            self.addAlgorithm( alg )
+            self.addAlgorithm(alg)
 
     def id(self):
         """
