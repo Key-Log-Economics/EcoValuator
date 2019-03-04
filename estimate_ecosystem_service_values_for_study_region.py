57d62cbe8dea5595a86bfd4a36d1e71e2574f58b
Erich Purpur <36010345+epurpur@users.noreply.github.com>
2019-03-04 07:40:33 -0500

    Add files via upload

--- estimate_ecosystem_service_values_for_study_region.py
+++ estimate_ecosystem_service_values_for_study_region.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 """
 /***************************************************************************
@@ -118,7 +118,6 @@ class EstimateEcosystemServiceValuesForStudyRegion(QgsProcessingAlgorithm):
                 self.tr('Place to save intermediate html file')
             )
         )
-        #maybe this should be QgsProcessingParameterFileDestination?
         self.addParameter(
             QgsProcessingParameterFeatureSink(
                 self.OUTPUT_ESV_TABLE,
@@ -358,7 +357,7 @@ class EstimateEcosystemServiceValuesForStudyRegion(QgsProcessingAlgorithm):
         lowercase alphanumeric characters only and no spaces or other
         formatting characters.
         """
-        return 'Step 1: Estimate Ecosystem Service Values (ESVs) for Study Region'
+        return 'Step 1: Estimate ecosystem service values for study region'
 
     def displayName(self):
         """
@@ -390,7 +389,7 @@ class EstimateEcosystemServiceValuesForStudyRegion(QgsProcessingAlgorithm):
         should provide a basic description about what the algorithm does and the
         parameters and outputs associated with it..
         """
-        return self.tr("Dev Branch of the Plugin")
+        return self.tr("This algorithm does 3 things:\n 1. Clips the Input NLCD raster by the user-supplied Input mask layer for the region of interest.\n 2. Calculates how much area each type of land cover accounts for in the now-clipped NLCD raster.\n 3. Multiplies those areas by each of the associated per-hectare ecosystem service values (ESV) in the Input table of ESV research data.\n Outputs include the clipped raster and a table of aggregate ESV for each land cover type in the study region. \n Once users have added the proper NLCD raster data* and their study region polygon layer* to the QGIS project, they have the following options: \n The input 2011 NLCD raster (https://www.mrlc.gov/nlcd2011.php)*: This is a copy of the National Land Cover Database (2011) depicting land cover in 16 types at 30m resolution for the conterminous US.\n Input mask layer*: This vector data should describe the user's area of interest or study region. The algorithm clips the NLCD data to this region. \n *Both the NLCD and input mask layer must be in CRS: EPSG: 102003 (https://epsg.io/102003) (USA Contiguous Albers Equal Area Conic) in order to successfully execute the clip to the mask layer. Users can download a copy of the NLCD data the proper CRS here (https://www.dropbox.com/sh/vz0c41vuqt6et9j/AACxahJHy55JUGvZPUsmhdWwa?dl=0). \n Input table of ESV research data: This table of data comes pre-loaded with the plugin and provides the per-hectare-per-year minimum, average, and maximum dollar value estimates for each land cover type and ecosystem service. These figures are also adjusted for the exchange rate and inflation. These figures are derived from an extensive literature review. See Help for details.\n Clipped raster layer: This output is the result of the clipping process that is the first step of the algorithm. \n Place to save intermediate html file: If you'd like to save an html file with the number of pixels and total area (in m^2) for each different pixel value in the input raster [This is optional].\n Output ESV table: This table contains NLCD land cover values and descriptions as rows, and associated ecosystem service values based on the minimum, mean, and maximum values per hectare from the ESV research data. Note that many NULL values will appear in the table due to a lack of existing research on certain ecosystem services in each land cover type, and NULL does not correspond to a dollar value of 0. It simply indicates a current lack of primary studies to determine the dollar amount. (See 'Help' for more information on the methods and sources behind the ESV table.)")
 
     def tr(self, string):
         return QCoreApplication.translate('Processing', string)
